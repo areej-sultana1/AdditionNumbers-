b33445b46df4c9e7533671370112416678340739
Areej Sultana <82303448+areej-sultana1@users.noreply.github.com>
2022-03-10 10:23:11 +0500

    Add files via upload

---
+++ precache-manifest.b6629d2296fdd3ae35ddfd99e65001f9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb634c32ea1b1f32eeb",
+    "url": "/Addition_numbers/static/css/main.eb9af1e1.chunk.css"
+  },
+  {
+    "revision": "deb634c32ea1b1f32eeb",
+    "url": "/Addition_numbers/static/js/main.1e32daac.chunk.js"
+  },
+  {
+    "revision": "172b7d6fde1ad7143d43",
+    "url": "/Addition_numbers/static/js/runtime~main.058de288.js"
+  },
+  {
+    "revision": "6b5973692395617b9ba0",
+    "url": "/Addition_numbers/static/css/2.7db094e6.chunk.css"
+  },
+  {
+    "revision": "6b5973692395617b9ba0",
+    "url": "/Addition_numbers/static/js/2.747e5e23.chunk.js"
+  },
+  {
+    "revision": "669f8cdb6aa1730b188e1d564f50d648",
+    "url": "/Addition_numbers/static/media/bar.669f8cdb.png"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/Addition_numbers/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/Addition_numbers/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/Addition_numbers/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/Addition_numbers/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/Addition_numbers/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "30400720d86656248ae3ee498fddb683",
+    "url": "/Addition_numbers/static/media/back.30400720.svg"
+  },
+  {
+    "revision": "efc0e3314bf0a9623a140aa5e147db9f",
+    "url": "/Addition_numbers/static/media/background.efc0e331.gif"
+  },
+  {
+    "revision": "046067690e7d1226cb1cb01aaa111810",
+    "url": "/Addition_numbers/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Addition_numbers/precache-manifest.b6629d2296fdd3ae35ddfd99e65001f9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Addition_numbers/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
